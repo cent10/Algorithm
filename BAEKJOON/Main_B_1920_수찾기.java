31efc209e96ab95b8ebbfe074f7023f233b311b6
cent10 <31956355+cent10@users.noreply.github.com>
2020-08-08 19:18:03 +0900

    Add files via upload

---
+++ BAEKJOON/Main_B_1920_수찾기.java
@@ -0,0 +1,35 @@
+// Silver IV - 1920 : 수 찾기
+
+import java.io.*;
+import java.util.*;
+
+public class Main_B_1920_수찾기 {
+
+	static int N, M;
+	static Map<Integer, Integer> map = new HashMap<>();
+	
+	public static void main(String[] args) throws Exception {
+		StringBuilder sb = new StringBuilder();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		for (int i = 0; i < N; i++) {
+			int num = Integer.parseInt(st.nextToken());
+			map.put(num, num);
+		}
+
+		M = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine(), " ");
+		for (int i = 0; i < M; i++) {
+			int num = Integer.parseInt(st.nextToken());
+			if (map.containsKey(num)) {
+				sb.append(1);
+			} else {
+				sb.append(0);
+			}
+			sb.append("\n");
+		}
+		
+		System.out.println(sb);
+	}// end of main
+}
